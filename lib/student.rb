524fc3e1efaf7eba5298c16ade61bafd20ea05d8
Matthew C <m23.cullen@gmail.com>
2019-10-16 15:54:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,21 @@ class Student
   end
   
   def self.create_table
-    sql = <<-SQL
-    \SQL
+    sql = <<-SQL 
+        CREATE TABLE IF NOT EXISTS students 
+      (id, INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
+      SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+    sql = <<-SQL 
+      
+    SQL
+  end 
+  
+  def self.create(name,grade)
+    student = self.new 
+    
   end 
 end
